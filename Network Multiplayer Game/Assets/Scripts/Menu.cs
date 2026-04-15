5f6f75c521b1dd8cb67c8841008ea1acc07d2ab8
liliyy-t <queenlilithatanou@gmail.com>
2026-04-15 22:30:09 +0200

    Menu UI done

--- Network Multiplayer Game/Assets/Scripts/Menu.cs
+++ Network Multiplayer Game/Assets/Scripts/Menu.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("UI")]
+    [SerializeField] private TMP_InputField ipInput;
+    [SerializeField] private TMP_InputField portInput;
+
+    [Header("Defaults")]
+    [SerializeField] private string defaultIP = "127.0.0.1";
+    [SerializeField] private ushort defaultPort = 7777;
+
+    [SerializeField] private UnityTransport transport;
+    [SerializeField] private NetworkManager networkManager;
+
+    private void Awake()
     {
-        
+        if (ipInput) ipInput.text = defaultIP;
+        if (portInput) portInput.text = defaultPort.ToString();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartHost()
     {
-        
+        ushort port = GetPort();
+        transport.SetConnectionData("0.0.0.0", port);
+
+        networkManager.SceneManager.LoadScene("EscapeRoom", LoadSceneMode.Single);
+    }
+
+    public void StartServerOnly()
+    {
+        ushort port = GetPort();
+        transport.SetConnectionData("0.0.0.0", port);
+        networkManager.StartServer();
+    }
+
+    public void JoinGame()
+    {
+        string ip = GetIP();
+        ushort port = GetPort();
+
+        transport.SetConnectionData(ip, port);
+        networkManager.StartClient();
+    }
+
+    private string GetIP()
+    {
+        if (!ipInput || string.IsNullOrWhiteSpace(ipInput.text))
+            return defaultIP;
+
+        return ipInput.text.Trim();
+    }
+
+    private ushort GetPort()
+    {
+        if (!portInput || !ushort.TryParse(portInput.text, out ushort port))
+            return defaultPort;
+
+        return port;
     }
 }
