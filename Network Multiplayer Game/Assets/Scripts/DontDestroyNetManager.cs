4706d585c0046b396aba6cb6584a27dc69e8de32
erinnivv <erin.govender09@gmail.com>
2026-04-28 13:49:35 +0200

    testing menu stuff

--- Network Multiplayer Game/Assets/Scripts/DontDestroyNetManager.cs
+++ Network Multiplayer Game/Assets/Scripts/DontDestroyNetManager.cs
@@ -4,6 +4,7 @@ public class DontDestroyNetManager : MonoBehaviour
 {
     void Awake()
     {
+        Debug.Log($"[DontDestroy] Awake called on {gameObject.name} in scene {gameObject.scene.name}");
         DontDestroyOnLoad(gameObject);
     }
 }
